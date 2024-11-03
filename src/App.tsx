125b13f5c28e837a0a2bc458dcbd44c0bd0a3e81
kevin <kai38547@gmail.com>
2024-11-03 01:16:39 -0400

    Add in CRUD for Vendor

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
 import './App.css'
-import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import VendorForm from "./Forms/VendorForm.tsx";
+import VendorForm from "./Forms/Vendor/VendorForm.tsx";
+import DisplayVendors from "./Forms/Vendor/DisplayVendors.tsx";
+import VendorEdit from "./Forms/Vendor/VendorEdit.tsx";
+import Home from "./Home/Home.tsx";
+import DisplayCertifications from "./Forms/Certification/DisplayCertifications.tsx";
+import CertificationEdit from "./Forms/Certification/CertificationEdit.tsx";
+import CertificationForm from "./Forms/Certification/CertificationForm.tsx";
 
 function App() {
   return (
     <>
         <Router>
             <Routes>
-                {/*<Route path="/" element={<VendorForm/>} />*/}
+                <Route path="/" element={<Home/>} />
+                <Route path="/vendors" element={<DisplayVendors/>} />
+                <Route path="/vendors/create" element={<VendorForm/>} />
+                <Route path="/vendors/edit/:id" element={<VendorEdit/>} />
+                <Route path="/certifications" element={<DisplayCertifications/>} />
+                <Route path="/certifications/create" element={<CertificationForm/>} />
+                <Route path="/certifications/edit/:id" element={<CertificationEdit/>} />
             </Routes>
         </Router>
     </>
---
+++ src/utilities/api/vendorApi.ts
@@ -0,0 +1,78 @@
+import {Vendor} from "../../Types/Vendor.ts";
+
+const baseURL = "http://127.0.0.1:8000/api";
+
+export async function getVendors() {
+    try {
+        const response = await fetch(`${baseURL}/vendors`);
+        const data = await response.json() as Vendor[];
+        console.log(data)
+        return {success: true, data}
+    } catch (error) {
+        return {success: false, error}
+    }
+}
+
+export async function getVendorById(id: string) {
+    try {
+        const response = await fetch(`${baseURL}/vendors/${id}`);
+        const data = await response.json() as Vendor;
+        console.log(data)
+        return {success: true, data}
+    } catch (error) {
+        return {success: false, error}
+    }
+}
+
+export async function createVendor(formData: Vendor) {
+    try {
+        const response = await fetch(`${baseURL}/vendors`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(formData)
+        });
+        if (response.ok) {
+            console.log('Vendor data submitted successfully:', await response.json());
+        } else {
+            console.error('Failed to submit vendor data:', response.statusText);
+        }
+    } catch (error) {
+        console.error('Error submitting vendor data:', error);
+    }
+}
+
+export async function updateVendor(vendorId: string, formData: Vendor) {
+    try {
+        const response = await fetch(`${baseURL}/vendors/${vendorId}`, {
+            method: 'PUT',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(formData)
+        });
+        if (response.ok) {
+            console.log('Vendor data updated successfully:', await response.json());
+        } else {
+            console.error('Failed to update vendor data:', response.statusText);
+        }
+    } catch (error) {
+        console.error('Error updating vendor data:', error);
+    }
+}
+
+export async function deleteVendor(vendorId: string) {
+    try {
+        const response = await fetch(`${baseURL}/vendors/${vendorId}`, {
+            method: 'DELETE',
+            headers: { 'Content-Type': 'application/json' }
+        });
+        if (response.ok) {
+            console.log('Vendor data deleted successfully:', await response.json());
+        } else {
+            console.error('Failed to delete vendor data:', response.statusText);
+        }
+    } catch (error) {
+        console.error('Error deleting vendor data:', error);
+    }
+}
+
+
+
