8941f538bedb817964edbff035cad2ec0a0ccff4
kevin <kai38547@gmail.com>
2024-11-03 01:59:55 -0400

    Add in CRUD for Component

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,9 @@ import Home from "./Home/Home.tsx";
 import DisplayCertifications from "./Forms/Certification/DisplayCertifications.tsx";
 import CertificationEdit from "./Forms/Certification/CertificationEdit.tsx";
 import CertificationForm from "./Forms/Certification/CertificationForm.tsx";
+import ComponentForm from "./Forms/Component/ComponentForm.tsx";
+import DisplayComponents from "./Forms/Component/DisplayComponents.tsx";
+import ComponentEdit from "./Forms/Component/ComponentEdit.tsx";
 
 function App() {
   return (
@@ -20,6 +23,9 @@ function App() {
                 <Route path="/certifications" element={<DisplayCertifications/>} />
                 <Route path="/certifications/create" element={<CertificationForm/>} />
                 <Route path="/certifications/edit/:id" element={<CertificationEdit/>} />
+                <Route path="/components" element={<DisplayComponents/>} />
+                <Route path="/components/create" element={<ComponentForm/>} />
+                <Route path="/components/edit/:id" element={<ComponentEdit/>} />
             </Routes>
         </Router>
     </>
